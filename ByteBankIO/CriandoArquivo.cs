3ad90413bcf42dcd9a551af6dc5d8a31a1e7401b
Cristeflucas <cristeflucas@hotmail.com>
2026-03-10 22:14:02 -0300

    Usando fileStream

---
+++ ByteBankIO/CriandoArquivo.cs
@@ -0,0 +1,17 @@
+﻿using ByteBankIO;
+using System.Text;
+partial class Program
+{
+    static void CriandoArquivo()
+    {
+        var enderecoDoArquivo = "contasExportadas.csv"; 
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7895, 4785.40, Pedro Silva";
+            var enconding = Encoding.UTF8;
+            var bytes = enconding.GetBytes(contaComoString);
+            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
+
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,26 @@
 ﻿using ByteBankIO;
+using System.Numerics;
+using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
-
+        CriandoArquivo();
         Console.ReadLine();
     }
+
+    /*static ContaCorrente ConvertendoTextoParaConta(string linha)
+    {
+        string [] campos = linha.Split(',');
+        int agencia = int.Parse(campos[0]);
+        int numero = int.Parse(campos[1]);
+        double saldo = double.Parse(campos[2]);
+        var titular = new Cliente();
+        titular.Nome = campos[3];
+        var conta = new ContaCorrente(agencia, numero);
+        conta.Depositar(saldo);
+        conta.Titular = titular;
+        return conta;
+    }*/
 }
